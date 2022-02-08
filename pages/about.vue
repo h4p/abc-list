ece0b824c788dca4a07950b0227d2f3653c95619
Tobias Wessels <twe@ongoingmedia.de>
2022-02-08 15:01:11 +0100

    Fill repo

---
+++ pages/about.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <p>My humble about page</p>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'AboutPage',
+}
+</script>
