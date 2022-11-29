3fec4c6149a402cc2290c9947819349afcd0b7b2
Tobias Wessels <tobias.wessels@ongoingmedia.de>
2022-11-29 22:35:25 +0100

    OnOff toggle

---
+++ pages/onoff.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>
+      {{ isOn ? 'On' : 'Off' }}
+    </h1>
+    <v-button @click="isOn = !isOn">
+      {{ isOn ? 'click to turn off' : 'click to turn on' }}
+    </v-button>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'OnOff',
+    data() {
+      return {
+        isOn: true,
+      };
+    }
+  }
+</script>
